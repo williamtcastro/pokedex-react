b4063e6013915df68792ef8e0e5c9aa9b92dec3a
William de Castro <williamtdcastro@gmail.com>
2019-02-08 10:13:05 -0200

    Update fot netlify

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Footer extends Component {
+    render(){
+        return(
+            <footer class="page-footer ">
+              <div class="text-center">© 2019 Pokédex React - William de Castro</div>
+            </footer>
+          );
+    }
+}
